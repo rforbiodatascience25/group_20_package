a052657138d9bbc5f2f3ffccf44ce0cfa26d930c
Lebimle <s235354@dtu.dk>
2025-10-28 21:15:16 +0100

    added further tests

--- tests/testthat/test-get_codons.R
+++ tests/testthat/test-get_codons.R
@@ -1,3 +1,4 @@
 test_that("Basic functionality default arguments", {
   expect_equal(get_codons("ATGATG"), c("ATG","ATG"))
+  expect_equal(get_codons("AUGGCUGGCCUA", start = 2), c("GCU","GGC","CUA"))
 })
